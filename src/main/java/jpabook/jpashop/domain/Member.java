d8f6ef245cfdc18b00b16409ed0cceab6ff74137
0830thgus@naver.com <0830thgus@naver.com>
2024-01-20 15:51:28 +0900

    JpaShop - 엔티티매핑

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+    @Id @GeneratedValue
+    @Column(name="MEMBER_ID")
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    private String zipCode;
+
+}
